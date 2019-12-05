c3ca71b5496a1bbc3dc63397a9b0e2df7013cb6f
Alex Russell <alexander.russell@sap.com>
2019-12-05 13:21:52 +0100

    Remove accidentally added file.

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo systemctl stop jenkins && mvn clean package -DskipTests && sudo cp target/audit-trail.hpi /var/lib/jenkins/plugins/audit-trail.jpi && sudo chown jenkins:jenkins /var/lib/jenkins/plugins/audit-trail.jpi && sudo systemctl start jenkins && sleep 5 && sudo systemctl status jenkins
