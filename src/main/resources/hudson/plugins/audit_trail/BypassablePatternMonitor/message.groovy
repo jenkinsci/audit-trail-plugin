7ff4803785d6324a4ee02c2889bccf876f67a26d
Wadeck Follonier <wadeck.follonier@gmail.com>
2020-11-10 11:09:23 +0100

    [JENKINS-63928] Prevent form name conflict

--- src/main/resources/hudson/plugins/audit_trail/BypassablePatternMonitor/message.groovy
+++ src/main/resources/hudson/plugins/audit_trail/BypassablePatternMonitor/message.groovy
@@ -12,7 +12,7 @@ dl {
         form(method: 'post', name: 'default', action: "${rootURL}/${monitor.url}/applyDefault") {
             input(name: 'default', type: 'submit', value: 'Apply default pattern', class: 'submit-button primary')
         }
-        form(method: 'get', name: 'config', action: "${rootURL}/${monitor.url}/redirectToConfig") {
+        form(method: 'get', name: 'redirect-to-config', action: "${rootURL}/${monitor.url}/redirectToConfig") {
             input(name: 'config', type: 'submit', value: 'Go to configuration', class: 'submit-button primary')
         }
         raw('<b>Found bypassable Audit Trail logging patterns:</b>')
