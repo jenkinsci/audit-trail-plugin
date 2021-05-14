1ef9659c0cddd243b8c22e702ef659efff01cc20
Pierre Beitz <pibeitz@gmail.com>
2021-05-14 22:00:43 +0200

    [Chore] Bump minimal version of Jenkins to 2.263

--- src/test/java/hudson/plugins/audit_trail/AuditTrailFilterTest.java
+++ src/test/java/hudson/plugins/audit_trail/AuditTrailFilterTest.java
@@ -8,7 +8,6 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.Util;
 import hudson.model.Cause;
 import hudson.model.FreeStyleProject;
-import org.apache.http.HttpStatus;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -54,32 +53,6 @@ public class AuditTrailFilterTest {
         assertTrue("logged actions: " + log, Pattern.compile(".*id=1.*job/test-job.*by \\Q127.0.0.1\\E.*", Pattern.DOTALL).matcher(log).matches());
     }
 
-    @Test
-    @Issue("SECURITY-1815")
-    public void requestWithSemiColumnIsProperlyLogged() throws Exception {
-        String logFileName = "security-1815.log";
-        File logFile = new File(tmpDir.getRoot(), logFileName);
-        JenkinsRule.WebClient wc = j.createWebClient();
-        new SimpleAuditTrailPluginConfiguratorHelper(logFile).sendConfiguration(j, wc);
-
-        WebRequest request = new WebRequest(new URL(wc.getContextPath()+ "quietDown/..;/") , HttpMethod.POST);
-        wc.addCrumb(request);
-
-        try {
-            wc.getPage(request);
-        } catch (FailingHttpStatusCodeException e) {
-            if(e.getStatusCode() != HttpStatus.SC_METHOD_NOT_ALLOWED) {
-                // when the plugin is moved to a Core implementing SECURITY-1815 this request will start returning with
-                // a 400 error code. Voluntarily rethrowing to have this fail,
-                // because this failing test will be the time to reconsider removing this specific dev
-                throw e;
-            }
-            // otherwise silently ignore, the endpoint returns a 405
-        }
-        String log = Util.loadFile(new File(tmpDir.getRoot(), logFileName + ".0"), StandardCharsets.UTF_8);
-        assertTrue("logged actions: " + log, log.contains("quietDown"));
-    }
-
     @Issue("JENKINS-15731")
     @Test
     public void createItemLogsTheNewItemName() throws Exception {
@@ -91,6 +64,7 @@ public class AuditTrailFilterTest {
         HtmlPage configure = wc.goTo("view/all/newJob");
         HtmlForm form = configure.getFormByName("createItem");
         form.getInputByName("name").setValueAttribute(jobName);
+        form.getInputByName("name").blur();
         // not clear to me why the input is not visible in the test (yet it exists in the page)
         // for some reason the two next calls are needed
         form.getInputByValue("hudson.model.FreeStyleProject").click(false, false, false, true, true, false);
