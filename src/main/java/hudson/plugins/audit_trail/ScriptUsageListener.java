9ba6c409dd80470db592b143dfc69b9e24c75da5
Pierre Beitz <pibeitz@gmail.com>
2025-08-16 16:09:51 +0200

    Adapt to the new ScriptListener API

--- src/main/java/hudson/plugins/audit_trail/ScriptUsageListener.java
+++ src/main/java/hudson/plugins/audit_trail/ScriptUsageListener.java
@@ -1,12 +1,19 @@
 package hudson.plugins.audit_trail;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import groovy.lang.Binding;
 import hudson.Extension;
+import hudson.cli.GroovyCommand;
+import hudson.cli.GroovyshCommand;
 import hudson.model.User;
+import hudson.util.RemotingDiagnostics;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
 import jenkins.model.Jenkins;
-import jenkins.model.ScriptListener;
+import jenkins.util.ScriptListener;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
@@ -22,29 +29,40 @@ import org.kohsuke.stapler.StaplerRequest;
 @Extension
 public class ScriptUsageListener implements ScriptListener {
     private static final Logger LOGGER = Logger.getLogger(ScriptUsageListener.class.getName());
+    private static final Map<Class<?>, String> FEATURE_MAPPING;
+
+    static {
+        FEATURE_MAPPING = Map.of(
+                RemotingDiagnostics.class, "Script Console Controller",
+                GroovyshCommand.class, "CLI/GroovySh",
+                GroovyCommand.class, "CLI/GroovyCommand");
+    }
 
     @Inject
     AuditTrailPlugin configuration;
 
     /**
-     * Called when a (privileged) groovy script is executed.
-     *
-     * @see Jenkins#_doScript(StaplerRequest, org.kohsuke.stapler.StaplerResponse, javax.servlet.RequestDispatcher, hudson.remoting.VirtualChannel, hudson.security.ACL)
-     * @param script The script to be executed.
-     * @param origin Descriptive identifier of the origin where the script is executed (Controller, Agent ID, Run ID).
-     * @param u If available, the user that executed the script. Can be null.
+     * @inheritDoc
      */
     @Override
-    public void onScript(String script, String origin, User u) {
+    public void onScriptExecution(
+            String script,
+            @CheckForNull Binding $,
+            @NonNull Object feature,
+            @CheckForNull Object $$,
+            @NonNull String $$$,
+            @CheckForNull User user) {
         if (!configuration.getLogScriptUsage()) {
             return;
         }
         StringBuilder builder = new StringBuilder();
 
-        if (u != null) {
-            builder.append(String.format("A groovy script was executed by user '%s'. Origin: %s. ", u.getId(), origin));
+        if (user != null) {
+            builder.append(String.format(
+                    "A groovy script was executed by user '%s'. Origin: %s. ",
+                    user.getId(), prettyPrintFeature(feature)));
         } else {
-            builder.append(String.format("A groovy script was executed. Origin: %s.", origin));
+            builder.append(String.format("A groovy script was executed. Origin: %s.", prettyPrintFeature(feature)));
         }
 
         builder.append("\nThe executed script: \n");
@@ -56,5 +74,13 @@ public class ScriptUsageListener implements ScriptListener {
         for (AuditLogger logger : configuration.getLoggers()) {
             logger.log(log);
         }
+    }
+
+    private String prettyPrintFeature(Object feature) {
+        if (feature instanceof Class<?>) {
+            return FEATURE_MAPPING.getOrDefault(feature, feature.toString());
+        }
+        // currently it seems the feature is always a class
+        return feature.toString();
     }
 }
